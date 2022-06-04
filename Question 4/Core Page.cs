e40922dcac803e8d3f5b015291dcc93d24d1ef58
OswaZahra <63903304+OswaZahra@users.noreply.github.com>
2022-06-04 20:46:51 +0500

    Add files via upload

---
+++ Question 4/Core Page.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    internal class Core_Page
+    {
+        public static IWebDriver driver;
+        public static IWebDriver SeleniumInit()
+        {
+            IWebDriver chromeDriver = new ChromeDriver();
+            driver = chromeDriver;
+            return driver;
+        }
+    }
+}
--- Question 4/Main.cs
+++ Question 4/Main.cs
@@ -1,1 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
 
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Main
+    {
+        //IWebDriver driver = new ChromeDriver();
+        SearchFunctionality testSearchFunctionality = new SearchFunctionality();
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Core_Page.SeleniumInit();
+            testSearchFunctionality.Test("https://www.airblue.com/");
+            Core_Page.driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            Core_Page.SeleniumInit();
+            testSearchFunctionality.Test2("https://www.airblue.com/");
+            Core_Page.driver.Close();
+        }
+
+
+
+    }
+}
